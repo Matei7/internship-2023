becb7320fb154a91a312e40195f8d051a938e4c5
Tomsa Alexandru <alexandru.tomsa@bitstone.eu>
2023-07-26 15:31:49 +0300

    The tasks are technically done, but the page looks ugly as sin.

---
+++ dist/index.js
@@ -0,0 +1,106 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/sass/styles.scss":
+/*!******************************!*\
+  !*** ./src/sass/styles.scss ***!
+  \******************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n// extracted by mini-css-extract-plugin\n\n\n//# sourceURL=webpack://internship-2023/./src/sass/styles.scss?");
+
+/***/ }),
+
+/***/ "./src/js/app.js":
+/*!***********************!*\
+  !*** ./src/js/app.js ***!
+  \***********************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   init: () => (/* binding */ init)\n/* harmony export */ });\nasync function init() {\r\n    await loadShopProducts();\r\n    setupAddToCartButtons();\r\n\r\n}\r\n\r\nasync function loadShopProducts() {\r\n    let productObjectsArray = (await getAllProductsJSON()).products;\r\n\r\n    console.log(productObjectsArray);\r\n    let productsContainer = document.getElementById(\"products-list\");\r\n\r\n    for (const product of productObjectsArray) {\r\n        let productHTML = getProductHTML(product);\r\n        productsContainer.appendChild(productHTML);\r\n    }\r\n}\r\n\r\nfunction getProductHTML(productObject)\r\n{\r\n    let productId = productObject[\"id\"];\r\n    let productTitle = productObject[\"title\"];\r\n    let productThumbnail = productObject[\"thumbnail\"];\r\n    let productDescription = productObject[\"description\"];\r\n    let productRating = productObject[\"rating\"];\r\n    let productPrice = productObject[\"price\"];\r\n\r\n    let productHTML = document.createElement('div');\r\n    productHTML.setAttribute('class', \"item-container\");\r\n    productHTML.setAttribute('data-id', productId);\r\n    productHTML.innerHTML = `\r\n\\t\\t\\t\\t<p class=\"item-title\">${productTitle}</p>\r\n\\t\\t\\t\\t<img class=\"item-thumbnail\" src=\"${productThumbnail}\" alt=\"placeholder image thumbnail\"/>\r\n\\t\\t\\t\\t<p class=\"item-description\">${productDescription}</p>\r\n\\t\\t\\t\\t<p class=\"item-rating\">${productRating}</p>\r\n\\t\\t\\t\\t<div class=\"item-purchase-details\">\r\n\\t\\t\\t\\t\\t<p class=\"item-price\">${productPrice}</p>\r\n\\t\\t\\t\\t\\t<button class=\"add-to-cart-button\">Add To Cart</button>\r\n\\t\\t\\t\\t</div>\r\n    `;\r\n\r\n    return productHTML;\r\n}\r\n\r\nasync function getAllProductsJSON() {\r\n    return fetch('https://dummyjson.com/products')\r\n        .then(res => {return res.json();});\r\n\r\n    /*return `\r\n    [\r\n        {\r\n            \"id\": 1,\r\n            \"title\": \"iPhone 9\",\r\n            \"description\": \"An apple mobile which is nothing like apple\",\r\n            \"price\": 549,\r\n            \"discountPercentage\": 12.96,\r\n            \"rating\": 4.69,\r\n            \"stock\": 94,\r\n            \"brand\": \"Apple\",\r\n            \"category\": \"smartphones\",\r\n            \"thumbnail\": \"https://i.dummyjson.com/data/products/1/thumbnail.jpg\",\r\n            \"images\": [\r\n                \"https://i.dummyjson.com/data/products/1/1.jpg\",\r\n                \"https://i.dummyjson.com/data/products/1/2.jpg\",\r\n                \"https://i.dummyjson.com/data/products/1/3.jpg\",\r\n                \"https://i.dummyjson.com/data/products/1/4.jpg\",\r\n                \"https://i.dummyjson.com/data/products/1/thumbnail.jpg\"\r\n            ]\r\n        },\r\n    ]\r\n    `*/\r\n}\r\n\r\nfunction setupAddToCartButtons()\r\n{\r\n    let addToCartButtons = document.getElementsByClassName(\"add-to-cart-button\");\r\n\r\n    console.log(addToCartButtons.length);\r\n\r\n    for(const button of addToCartButtons)\r\n    {\r\n        button.addEventListener('click', function (event){\r\n            button.textContent = \"Added to cart\";\r\n            button.style.setProperty(\"background-color\", \"var(--green1)\");\r\n\r\n            let newPopup = createAddToCartPopup();\r\n            document.getElementById('app').appendChild(newPopup);\r\n\r\n            setTimeout(function (){\r\n                button.textContent = \"Add to cart\";\r\n                button.style.setProperty(\"background-color\", \"var(--yellow1)\");\r\n                newPopup.remove();\r\n            }, 7000);\r\n        });\r\n    }\r\n}\r\n\r\nfunction createAddToCartPopup(text=\"The product has been added to your cart successfully!\"){\r\n    let newPopup = document.createElement('div');\r\n    let popupContent = document.createElement('div');\r\n    newPopup.appendChild(popupContent);\r\n    newPopup.setAttribute('class', 'add-to-cart-popup');\r\n    popupContent.innerText = text;\r\n\r\n    return newPopup;\r\n}\n\n//# sourceURL=webpack://internship-2023/./src/js/app.js?");
+
+/***/ }),
+
+/***/ "./src/js/index.js":
+/*!*************************!*\
+  !*** ./src/js/index.js ***!
+  \*************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _sass_styles_scss__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../sass/styles.scss */ \"./src/sass/styles.scss\");\n/* harmony import */ var _app__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./app */ \"./src/js/app.js\");\n\r\n\r\n\r\n(0,_app__WEBPACK_IMPORTED_MODULE_1__.init)();\r\n\n\n//# sourceURL=webpack://internship-2023/./src/js/index.js?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/define property getters */
+/******/ 	(() => {
+/******/ 		// define getter functions for harmony exports
+/******/ 		__webpack_require__.d = (exports, definition) => {
+/******/ 			for(var key in definition) {
+/******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
+/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
+/******/ 				}
+/******/ 			}
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
+/******/ 	(() => {
+/******/ 		__webpack_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	(() => {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = (exports) => {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module can't be inlined because the eval devtool is used.
+/******/ 	var __webpack_exports__ = __webpack_require__("./src/js/index.js");
+/******/ 	
+/******/ })()
+;
--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,106 @@
-export function init() {
-	const appElement = document.querySelector( '#app' );
-	appElement.innerHTML = 'Hello World!';
-	appElement.classList.add( 'with-bg' );
+export async function init() {
+    await loadShopProducts();
+    setupAddToCartButtons();
+
+}
+
+async function loadShopProducts() {
+    let productObjectsArray = (await getAllProductsJSON()).products;
+
+    console.log(productObjectsArray);
+    let productsContainer = document.getElementById("products-list");
+
+    for (const product of productObjectsArray) {
+        let productHTML = getProductHTML(product);
+        productsContainer.appendChild(productHTML);
+    }
+}
+
+function getProductHTML(productObject)
+{
+    let productId = productObject["id"];
+    let productTitle = productObject["title"];
+    let productThumbnail = productObject["thumbnail"];
+    let productDescription = productObject["description"];
+    let productRating = productObject["rating"];
+    let productPrice = productObject["price"];
+
+    let productHTML = document.createElement('div');
+    productHTML.setAttribute('class', "item-container");
+    productHTML.setAttribute('data-id', productId);
+    productHTML.innerHTML = `
+\t\t\t\t<p class="item-title">${productTitle}</p>
+\t\t\t\t<img class="item-thumbnail" src="${productThumbnail}" alt="placeholder image thumbnail"/>
+\t\t\t\t<p class="item-description">${productDescription}</p>
+\t\t\t\t<p class="item-rating">${productRating}</p>
+\t\t\t\t<div class="item-purchase-details">
+\t\t\t\t\t<p class="item-price">${productPrice}</p>
+\t\t\t\t\t<button class="add-to-cart-button">Add To Cart</button>
+\t\t\t\t</div>
+    `;
+
+    return productHTML;
+}
+
+async function getAllProductsJSON() {
+    return fetch('https://dummyjson.com/products')
+        .then(res => {return res.json();});
+
+    /*return `
+    [
+        {
+            "id": 1,
+            "title": "iPhone 9",
+            "description": "An apple mobile which is nothing like apple",
+            "price": 549,
+            "discountPercentage": 12.96,
+            "rating": 4.69,
+            "stock": 94,
+            "brand": "Apple",
+            "category": "smartphones",
+            "thumbnail": "https://i.dummyjson.com/data/products/1/thumbnail.jpg",
+            "images": [
+                "https://i.dummyjson.com/data/products/1/1.jpg",
+                "https://i.dummyjson.com/data/products/1/2.jpg",
+                "https://i.dummyjson.com/data/products/1/3.jpg",
+                "https://i.dummyjson.com/data/products/1/4.jpg",
+                "https://i.dummyjson.com/data/products/1/thumbnail.jpg"
+            ]
+        },
+    ]
+    `*/
+}
+
+function setupAddToCartButtons()
+{
+    let addToCartButtons = document.getElementsByClassName("add-to-cart-button");
+
+    console.log(addToCartButtons.length);
+
+    for(const button of addToCartButtons)
+    {
+        button.addEventListener('click', function (event){
+            button.textContent = "Added to cart";
+            button.style.setProperty("background-color", "var(--green1)");
+
+            let newPopup = createAddToCartPopup();
+            document.getElementById('app').appendChild(newPopup);
+
+            setTimeout(function (){
+                button.textContent = "Add to cart";
+                button.style.setProperty("background-color", "var(--yellow1)");
+                newPopup.remove();
+            }, 7000);
+        });
+    }
+}
+
+function createAddToCartPopup(text="The product has been added to your cart successfully!"){
+    let newPopup = document.createElement('div');
+    let popupContent = document.createElement('div');
+    newPopup.appendChild(popupContent);
+    newPopup.setAttribute('class', 'add-to-cart-popup');
+    popupContent.innerText = text;
+
+    return newPopup;
 }
