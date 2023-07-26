698b11f92afc94b37dfd5e0a8ab29088500a65be
Tomsa Alexandru <alexandru.tomsa@bitstone.eu>
2023-07-26 17:22:58 +0300

    It looks slightly better now. I also added the discount as part of the page

--- src/js/app.js
+++ src/js/app.js
@@ -1,14 +1,14 @@
 export async function init() {
     await loadShopProducts();
     setupAddToCartButtons();
-
 }
 
 async function loadShopProducts() {
     let productObjectsArray = (await getAllProductsJSON()).products;
 
-    console.log(productObjectsArray);
     let productsContainer = document.getElementById("products-list");
+
+
 
     for (const product of productObjectsArray) {
         let productHTML = getProductHTML(product);
@@ -24,17 +24,19 @@ function getProductHTML(productObject)
     let productDescription = productObject["description"];
     let productRating = productObject["rating"];
     let productPrice = productObject["price"];
+    let productDiscount = productObject["discountPercentage"];
+    let productPriceWithDiscount = productPrice * (100 - productDiscount) / 100;
 
     let productHTML = document.createElement('div');
     productHTML.setAttribute('class', "item-container");
     productHTML.setAttribute('data-id', productId);
     productHTML.innerHTML = `
-\t\t\t\t<p class="item-title">${productTitle}</p>
 \t\t\t\t<img class="item-thumbnail" src="${productThumbnail}" alt="placeholder image thumbnail"/>
+\t\t\t\t<p class="item-title">${productTitle}</p>
 \t\t\t\t<p class="item-description">${productDescription}</p>
-\t\t\t\t<p class="item-rating">${productRating}</p>
+\t\t\t\t<p class="item-rating">Rating: ${productRating}/5.00</p>
 \t\t\t\t<div class="item-purchase-details">
-\t\t\t\t\t<p class="item-price">${productPrice}</p>
+\t\t\t\t\t<p class="item-price">Price: <s>$${productPrice}</s>   <span style="color: red">$${productPriceWithDiscount.toFixed(2)}</span></p>
 \t\t\t\t\t<button class="add-to-cart-button">Add To Cart</button>
 \t\t\t\t</div>
     `;
@@ -42,53 +44,30 @@ function getProductHTML(productObject)
     return productHTML;
 }
 
-async function getAllProductsJSON() {
+function getAllProductsJSON() {
     return fetch('https://dummyjson.com/products')
         .then(res => {return res.json();});
-
-    /*return `
-    [
-        {
-            "id": 1,
-            "title": "iPhone 9",
-            "description": "An apple mobile which is nothing like apple",
-            "price": 549,
-            "discountPercentage": 12.96,
-            "rating": 4.69,
-            "stock": 94,
-            "brand": "Apple",
-            "category": "smartphones",
-            "thumbnail": "https://i.dummyjson.com/data/products/1/thumbnail.jpg",
-            "images": [
-                "https://i.dummyjson.com/data/products/1/1.jpg",
-                "https://i.dummyjson.com/data/products/1/2.jpg",
-                "https://i.dummyjson.com/data/products/1/3.jpg",
-                "https://i.dummyjson.com/data/products/1/4.jpg",
-                "https://i.dummyjson.com/data/products/1/thumbnail.jpg"
-            ]
-        },
-    ]
-    `*/
 }
 
 function setupAddToCartButtons()
 {
     let addToCartButtons = document.getElementsByClassName("add-to-cart-button");
 
-    console.log(addToCartButtons.length);
 
     for(const button of addToCartButtons)
     {
         button.addEventListener('click', function (event){
             button.textContent = "Added to cart";
-            button.style.setProperty("background-color", "var(--green1)");
+            button.style.setProperty("background-color", "var(--button-pressed)");
 
-            let newPopup = createAddToCartPopup();
+            let buttonID = button.parentElement.parentElement.getAttribute('data-id');
+            let buttonTitle = button.parentElement.parentElement.firstElementChild.nextElementSibling.innerText;
+            let newPopup = createAddToCartPopup(`The product with ID: ${buttonID} and title: ${buttonTitle} has been added to your cart successfully`);
             document.getElementById('app').appendChild(newPopup);
 
             setTimeout(function (){
                 button.textContent = "Add to cart";
-                button.style.setProperty("background-color", "var(--yellow1)");
+                button.style.removeProperty("background-color");
                 newPopup.remove();
             }, 7000);
         });
