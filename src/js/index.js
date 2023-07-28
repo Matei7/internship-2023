93673541dfa5ab745a9c4ef5beb46100a4646c07
Laura Trufelea <laura.trufelea@bitstone.eu>
2023-07-29 00:25:07 +0300

    finished

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,7 @@
 import '../sass/styles.scss';
 import '../sass/check-out.scss';
 import { init } from "./app";
+//import {showCheckoutProducts} from "./app.js";
 
-
+//showCheckoutProducts();
 init();
