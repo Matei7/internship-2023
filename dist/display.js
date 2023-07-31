62577ae1d771a9da33d87fad1cd2fe9b5e5821fd
bitstone-alexandru-negru <acon3331@yahoo.com>
2023-07-31 18:09:08 +0300

    Fetch data from the internet

--- dist/display.js
+++ dist/display.js
@@ -3,33 +3,33 @@ function addToCart(element){
 }
 
 let lastID = 1000;
+let allItems = [];
 
-function addAnItem(){
+function addAnItem(itemData){
     const itemToAdd = document.createElement("div");
     itemToAdd.classList.add("shop-item");
-    itemToAdd.id = lastID;
-    lastID++;
+    itemToAdd.id = itemData.id;
 
     const newItemCategory = document.createElement("div");
     newItemCategory.classList.add("item-category");
-    newItemCategory.innerHTML="##CATEGORY##";
+    newItemCategory.innerHTML=itemData.category;
 
     const newItemIMG = document.createElement("img");
     let picURL = "https://picsum.photos/30";
     picURL += String(Math.floor(Math.random() * 9));
     picURL += "/40";
     picURL += String(Math.floor(Math.random() * 9));
-    newItemIMG.src=picURL;
+    newItemIMG.src=itemData.thumbnail;
     newItemIMG.alt="Product for sale.";
     newItemIMG.classList.add("item-image");
 
     const newItemTitle = document.createElement("div");
     newItemTitle.classList.add("item-title");
-    newItemTitle.innerHTML="##TITLE##";
+    newItemTitle.innerHTML=itemData.title;
 
     const newItemPrice = document.createElement("div");
     newItemPrice.classList.add("item-price");
-    newItemPrice.innerHTML="##PRICE##";
+    newItemPrice.innerHTML="$"+itemData.price;
 
     const newItemCart = document.createElement("div");
     newItemCart.classList.add("item-add-to-cart");
@@ -45,10 +45,23 @@ function addAnItem(){
     document.getElementById("list-of-products").append(itemToAdd);
 }
 
-function addItems(howMany){
-    for(i = 0; i < howMany; i++){
-        addAnItem();
+function importItems() {
+    fetch('https://dummyjson.com/products')
+        .then(response => response.json())
+        .then(data => {
+            allItems = data;
+            displayItems(allItems);
+        })
+        .catch(error => {
+            console.error('Error fetching data:', error);
+        });
+}
+
+function displayItems(items){
+    for (i=0; i<items.products.length; i++){
+        let currentItem = items.products[i];
+        addAnItem(currentItem);
     }
 }
 
-addItems(20);
+importItems();
