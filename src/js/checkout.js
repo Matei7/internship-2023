82ff632abf062db4760a8a440c932d2a529dbc17
Laura Trufelea <laura.trufelea@bitstone.eu>
2023-07-31 10:28:01 +0300

    31iul

--- src/js/checkout.js
+++ src/js/checkout.js
@@ -1,14 +1,40 @@
 let checkoutWindow = document.querySelector(".checkout-window__grid");
 let totalText = document.querySelector(".checkout-window-total");
+
+let productsArrayAfterUpdateAProduct = [];
+
 async function updateTotalText() {
     try {
-        const total = await fetchTotal();
-        totalText.textContent = `Total: ${total.toFixed(2)}`;
+        const total = fetchCartsTotalPrice().then(total => {
+            totalText.textContent = `Total: ${total.toFixed(2)}`;
+        });
     } catch (error) {
         console.error('Error updating total text:', error);
     }
 }
 
+function fetchCartsTotalPrice() {
+    return fetch('https://vlad-matei.thrive-dev.bitstoneint.com/wp-json/internship-api/v1/cart/64c3a491accd0', {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+        }
+    })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(`Request failed with status ${response.status}`);
+            }
+            return response.json();
+        })
+        .then(data => {
+            const total = data.products.reduce((acc, product) => acc + product.total, 0);
+            return total;
+        })
+        .catch(error => {
+            console.error('Error fetching cart products:', error);
+            return 0; // În caz de eroare, returnăm 0 pentru total
+        });
+}
 function fetchCart() {
     return fetch('https://vlad-matei.thrive-dev.bitstoneint.com/wp-json/internship-api/v1/cart/64c3a491accd0', {
         method: 'GET',
@@ -31,62 +57,28 @@ function fetchCart() {
         });
 }
 
-function fetchTotal() {
-    return fetch('https://vlad-matei.thrive-dev.bitstoneint.com/wp-json/internship-api/v1/cart/64c3a491accd0', {
-        method: 'GET',
-        headers: {
-            'Content-Type': 'application/json',
-        }
-    })
-        .then(response => {
-            if (!response.ok) {
-                throw new Error(`Request failed with status ${response.status}`);
-            }
-            return response.json();
-        })
-        .then(data => {
-            return data.total;
-        })
-        .catch(error => {
-            console.error('Error fetching cart products:', error);
-            return [];
-        });
-}
 
-
-function plusMinusButtonsListener(card) {
-    const minusButton = document.querySelector(".minus");
-    const plusButton = document.querySelector(".plus");
-    const input = document.querySelector(".input-text");
-    minusButton.addEventListener("click", function () {
-        input.value--;
-        console.log("aici");
-    });
-    plusButton.addEventListener("click", function () {
-        input.value++;
-        console.log("aici");
-    });
-}
 
 function createItemBoxCheckout(product){
     let boxItem = document.createElement("div");
     boxItem.classList.add("checkout-window__grid__box-item");
     boxItem.setAttribute("data-id", product.id);
+    const price=(product.price - (product.price * product.discountPercentage) / 100).toFixed(2);
     let hmtlPart= `
         <img src=${product.thumbnail} alt="Product Image" class="box-item__image">
         <p class="box-item__title">${product.title}</p>
-        <p class="box-item__price">${product.discountedPrice.toFixed(2)}</p>
+        <p class="box-item__price">${price}</p>
         <div class="quantity buttons_added">
           <input type="button" value="-" class="minus">
           <input type="number" step="1" min="1" max="" value=${product.quantity}  class="input-text qty text" size="4" >
           <input type="button" value="+" class="plus">
         </div>
-        <p class="box-item__total-price"> ${(product.quantity * (product.price - (product.price * product.discountPercentage) / 100)).toFixed(2)}</p>
+        <p class="box-item__total-price"> ${(product.quantity * price).toFixed(2)}</p>
       </div>`;
     boxItem.innerHTML = hmtlPart;
     return boxItem;
-
 }
+
 function plusMinusBtnListener(card){
     const plusBtn = card.querySelector(".plus");
     const minusBtn = card.querySelector(".minus");
@@ -94,13 +86,14 @@ function plusMinusBtnListener(card){
     plusBtn.addEventListener("click", () => {
         quantity.value++;
         increaseQuantityRequest(card);
-        showCheckoutProducts();
+        updateQuantityProducts(card);
 
     });
     minusBtn.addEventListener("click", () => {
         if(quantity.value>1){
             quantity.value--;
-            //implemnt decrease quantity request---------------------------
+            decreaseQuantityRequest(card);
+            updateQuantityProducts(card);
         }
     });
 }
@@ -125,26 +118,55 @@ function increaseQuantityRequest(cardElement) {
 
 }
 
+function decreaseQuantityRequest(cardElement) {
+
+    const productId = Number(cardElement.dataset.id);
+    fetch('https://vlad-matei.thrive-dev.bitstoneint.com/wp-json/internship-api/v1/cart/64c3a491accd0', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            products: [
+                {
+                    id: productId,
+                    quantity: -1,
+                }
+            ]
+        })
+    })
+        .then(res => res.json())
+        .then(console.log);
+}
+
+function updateQuantityProducts(){
+    fetchCart()
+        .then(productsFromCart => {
+            for (const product of productsFromCart) {
+                console.log(product);
+                const itemBox = document.querySelector(`.checkout-window__grid__box-item[data-id="${product.id}"]`);
+                const itemQuantity = itemBox.querySelector(".input-text.qty.text");
+                const itemTotalPrice = itemBox.querySelector(".box-item__total-price");
+                itemQuantity.textContent = product.quantity;
+                const price=product.price - (product.price * product.discountPercentage) / 100;
+                itemTotalPrice.textContent = `${(product.quantity * price).toFixed(2)}`;
+                updateTotalText();
+            }
+        })
+        .catch(error => {
+            console.error('Error show cart items:', error);
+        });
+}
+
 
 function showCheckoutProducts(){
     fetchCart()
         .then(productsFromCart => {
             for (const product of productsFromCart) {
                 console.log(product);
-                if(document.querySelector(`.checkout-window__grid__box-item[data-id="${product.id}"]`) != null){
-                    const itemBox = document.querySelector(`.checkout-window__grid__box-item[data-id="${product.id}"]`);
-                    const itemQuantity = itemBox.querySelector(".input-text.qty.text");
-                    const itemTotalPrice = itemBox.querySelector(".box-item__total-price");
-                    itemQuantity.textContent = product.quantity;
-                    itemTotalPrice.textContent = `${(product.quantity * (product.price - (product.price * product.discountPercentage) / 100)).toFixed(2)}`;
-                }
-                else{
-                    let card=createItemBoxCheckout(product);
-                    checkoutWindow.appendChild(card);
-                    plusMinusBtnListener(card);
-                    updateTotalText();
-
-                }
+                let card=createItemBoxCheckout(product);
+                checkoutWindow.appendChild(card);
+                plusMinusBtnListener(card);
+                updateTotalText();
             }
         })
         .catch(error => {
